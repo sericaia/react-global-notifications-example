d27260b36dd6f7dd7e5fe0bbeca9cfa4cd715b40
Daniela Borges Matos de Carvalho <alunassertiva@gmail.com>
2020-01-10 16:01:50 +0000

    fix: tests

--- src/components/APIErrorNotification/APIErrorNotification.test.js
+++ src/components/APIErrorNotification/APIErrorNotification.test.js
@@ -1,21 +1,47 @@
 import React from "react";
-import { fireEvent } from "react-testing-library";
+import { fireEvent, wait, cleanup } from "@testing-library/react";
 import renderComponent from "../../common/test-utils/render-component";
 import APIErrorNotification from "./";
 
-it("should remove error when handleSubmit is pressed", () => {
-  const ERROR_MESSAGE = "SOME_ERROR";
-  const { getByTestId, removeErrorSpy } = renderComponent(
-    <APIErrorNotification />,
-    {
-      error: {
-        message: ERROR_MESSAGE
+describe("<APIErrorNotification />", () => {
+  afterEach(cleanup);
+
+  it("should remove error when handleSubmit is pressed", async () => {
+    const ERROR_MESSAGE = "SOME_ERROR";
+    const { getByTestId, removeErrorSpy } = renderComponent(
+      <APIErrorNotification />,
+      {
+        error: {
+          message: ERROR_MESSAGE
+        }
       }
-    }
-  );
+    );
 
-  const submitModalBtn = getByTestId("notification-submit-button");
-  fireEvent.click(submitModalBtn);
+    wait(() => {
+      const submitModalBtn = getByTestId("notification-submit-button");
+      fireEvent.click(submitModalBtn);
 
-  expect(removeErrorSpy).toHaveBeenCalled();
+      expect(removeErrorSpy).toHaveBeenCalled();
+    });
+  });
+
+  it("should not show modal when handleSubmit is pressed", async () => {
+    const ERROR_MESSAGE = "SOME_ERROR";
+    const { getByTestId, queryByTestId } = renderComponent(
+      <APIErrorNotification />,
+      {
+        error: {
+          message: ERROR_MESSAGE
+        }
+      }
+    );
+
+    wait(() => {
+      const submitModalBtn = getByTestId("notification-submit-button");
+      fireEvent.click(submitModalBtn);
+
+      const modal = queryByTestId("notification-modal");
+      expect(modal).toBeNull();
+    });
+  });
 });
